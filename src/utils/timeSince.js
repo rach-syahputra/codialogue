969daed9f8d52d6dca409b0e889a0f6d58ffe74a
Rach Syahputra <rach.syahputra@gmail.com>
2024-07-01 17:11:12 +0800

    feat(utils): Added timeSince util to convert createdAt into user-friendly time format

---
+++ src/utils/timeSince.js
@@ -0,0 +1,33 @@
+function timeSince(date) {
+  const now = new Date()
+  const posted = new Date(date)
+  const diff = now - posted
+  const diffYears = Math.floor(diff / (1000 * 60 * 60 * 24 * 365))
+  const diffMonths = Math.floor(diff / (1000 * 60 * 60 * 24 * 30))
+  const diffDays = Math.floor(diff / (1000 * 60 * 60 * 24))
+  const diffHours = Math.floor(diff / (1000 * 60 * 60))
+  const diffMinutes = Math.floor(diff / (1000 * 60))
+  const diffSeconds = Math.floor(diff / 1000)
+
+  if (diffYears > 0) {
+    return `${diffYears} years ago`
+  }
+  if (diffMonths > 0) {
+    return `${diffMonths} months ago`
+  }
+  if (diffDays > 0) {
+    return `${diffDays} days ago`
+  }
+  if (diffHours > 0) {
+    return `${diffHours} hours ago`
+  }
+  if (diffMinutes > 0) {
+    return `${diffMinutes} minutes ago`
+  }
+  if (diffSeconds > 0) {
+    return `${diffSeconds} seconds ago`
+  }
+  return 'just now'
+}
+
+export default timeSince
