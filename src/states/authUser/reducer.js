44bef84d9968b165931a8c08326f464892fa4415
Rach Syahputra <rach.syahputra@gmail.com>
2024-07-03 18:22:32 +0800

    feat: Change authUser reducer initialState to null

--- src/states/authUser/reducer.js
+++ src/states/authUser/reducer.js
@@ -1,8 +1,8 @@
 import { ActionType } from './action'
 
-const authUserFromStorage = JSON.parse(localStorage.getItem('auth-user'))
+// const authUserFromStorage = JSON.parse(localStorage.getItem('auth-user'))
 
-function authUserReducer(authUser = authUserFromStorage, action = {}) {
+function authUserReducer(authUser = null, action = {}) {
   switch (action.type) {
     case ActionType.SET_AUTH_USER:
       return action.payload.authUser
