0e6f025ab4900e892eab4ae551b1f14f486e0a07
Rach Syahputra <rach.syahputra@gmail.com>
2024-07-03 18:29:20 +0800

    feat: Integrate asyncPreloadProcess into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
 import { Route, Routes } from 'react-router-dom'
-import React from 'react'
+import React, { useEffect } from 'react'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import ThreadDetailPage from './pages/ThreadDetailPage'
 import CreateThreadPage from './pages/CreateThreadPage'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import LeaderboardsPage from './pages/LeaderboardsPage'
+import { asyncPreloadProcess } from './states/isPreload/action'
 
 function App() {
   const { isPreload = false } = useSelector((state) => state)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(asyncPreloadProcess())
+  }, [dispatch])
 
   if (isPreload) {
-    null
+    return null
   }
 
   return (
     <>
       <div className='app-container'>
-        <header>{/* navigation */}</header>
-        <main>
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/threads/new-thread' element={<CreateThreadPage />} />
-            <Route path='/threads/:id' element={<ThreadDetailPage />} />
-            <Route path='/leaderboards' element={<LeaderboardsPage />} />
-          </Routes>
-        </main>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/threads/new-thread' element={<CreateThreadPage />} />
+          <Route path='/threads/:id' element={<ThreadDetailPage />} />
+          <Route path='/leaderboards' element={<LeaderboardsPage />} />
+        </Routes>
       </div>
     </>
   )
